0b53406f70c8f60bd3e44db15dfef08a111feb31
Wouter Adem <wouter.adem@gmail.com>
2016-07-19 13:36:43 +0200

    Adding another install function to test.

--- src/AcquiaBLT/AcquiaBLTInstall.php
+++ src/AcquiaBLT/AcquiaBLTInstall.php
@@ -6,12 +6,16 @@ use Composer\Script\Event;
 
 class AcquiaBLTInstall
 {
-
+  // Occurs after install command has been executed with a lock file present.
   public static function postInstall(Event $event) {
     $event->getIO()->write("Post install");
   }
 
   public static function postPackageInstall(Event $event) {
     $event->getIO()->write("Post package install");
+  }
+
+  public static function postRootPackageInstall(Event $event) {
+    $event->getIO()->write("Post Root Package install");
   }
 }
